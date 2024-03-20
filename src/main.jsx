e9baf6904c877306f5ca531be2e83863d576c7a1
Syed Maaz <suyedmuhummadmaaz@gmail.com>
2024-03-20 21:49:27 +0500

    Resolved dependency conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import App from "./App";
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom"; 
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { BrowserRouter } from "react-router-dom";
